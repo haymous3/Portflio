606b2ec4fb4534b201f132d3610b8fc43826cdc0
Amos <adeyemoamos33@gmail.com>
2023-03-05 19:30:06 -0800

    animation done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 const NavBar = document.querySelector('#bars');
 const NavLinks = document.querySelector('#navLinks')
+const text = document.querySelector('#text')
 
 
 
@@ -20,3 +21,21 @@ const toogleNavs = () => {
 
 
 LoadEvenListerners()
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = 'AMOS'
+        text.style.fontSize = '4.5rem'
+        
+       
+    }, 0);
+    setTimeout(() => {
+        text.textContent = 'DEVELOPER'
+    }, 5000);
+    setTimeout(() => {
+        text.textContent = 'DESIGNER' 
+    }, 10000);
+}
+
+textLoad()
+setInterval(textLoad, 12000)
